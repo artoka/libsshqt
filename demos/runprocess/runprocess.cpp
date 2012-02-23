5646fc5c2606321900f7b70107fc19d9cc64df79
Arto Karppinen <arto.karppinen@iki.fi>
2012-02-23 13:26:00 +0200

    Use LibsshQtQuestionConsole in runprocess demo

--- demos/runprocess/runprocess.cpp
+++ demos/runprocess/runprocess.cpp
@@ -4,15 +4,16 @@
 #include <QUrl>
 
 #include "runprocess.h"
+#include "libsshqtquestionconsole.h"
 
 void RunProcess::runProcess()
 {
     QStringList args = qApp->arguments();
 
-    if ( args.count() != 4 ) {
+    if ( args.count() != 3) {
         qDebug() << "Usage:"
                  << qPrintable(args.at(0))
-                 <<"SSH_URL PASSWORD COMMAND";
+                 <<"SSH_URL COMMAND";
         qDebug() << "Example:"
                  << qPrintable(args.at(0))
                  << QString("ssh://user@hostname/")
@@ -25,8 +26,10 @@ void RunProcess::runProcess()
 
     client = new LibsshQtClient(this);
     client->setUrl(url);
-    client->usePasswordAuth(true, args.at(2));
+    client->useDefaultAuths();
     client->connectToHost();
 
-    process = client->runCommand(args.at(3));
+    new LibsshQtQuestionConsole(client);
+
+    process = client->runCommand(args.at(2));
 }
